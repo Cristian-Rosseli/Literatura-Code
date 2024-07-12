e4fb080a8083be0f79417863fd3f0c5a535362c7
Cristian Rosseli <cristianhenrique17@hotmail.com>
2024-07-12 17:28:05 -0300

    literalura

---
+++ src/main/java/com/alura/LiterAlura/model/LivroDados.java
@@ -0,0 +1,14 @@
+package com.alura.LiterAlura.model;
+
+import com.alura.LiterAlura.model.AutorDados;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record LivroDados(@JsonAlias("title") String titulo,
+                         @JsonAlias("authors") List<AutorDados> authors,
+                         @JsonAlias("languages") List<String> idioma,
+                         @JsonAlias("download_count") Double numeroDownload) {
+}
